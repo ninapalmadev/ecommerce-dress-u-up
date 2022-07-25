40ed7f30cd89dd293563bf53f97481bf81e23f41
ninapalmadev <ninapalmadev@gmail.com>
2022-07-25 12:21:03 -0300

    cria erros personalizados no cadastro

--- js/validacao.js
+++ js/validacao.js
@@ -17,7 +17,15 @@ const mensagensDeErro = {
         valueMissing: 'O campo nome não pode estar vazio.'
     },
     email: {
-        valueMissing: 'O campo email não pode estar vazio.'
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        customError: 'Você deve ser maior de 18 anos para se cadastrar.'
     },
 }
 
