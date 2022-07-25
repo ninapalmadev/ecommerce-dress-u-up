ef3b402f29c166a972ff3fbadac3195c16f92e2f
ninapalmadev <ninapalmadev@gmail.com>
2022-07-25 12:53:37 -0300

    cria função para preencher campos de endereço

--- js/validacao.js
+++ js/validacao.js
@@ -137,15 +137,27 @@ function recuperarCEP(input) {
         }
     };
 
-    if(!input.validity.patternMismatch && !input.validity.valueMissing){
+    if (!input.validity.patternMismatch && !input.validity.valueMissing) {
         fetch(url, options)
-        .then(response => response.json())
-        .then(data => {
-            if(data.erro){
-                input.setCustomValidity('Não foi possível buscar o CEP');
+            .then(response => response.json())
+            .then(data => {
+                if (data.erro) {
+                    input.setCustomValidity('Não foi possível buscar o CEP');
+                    return
+                }
+                input.setCustomValidity('');
+                preencheCamposComCEP(data);
                 return
-            }
-            
-        })
+            });
     }
 };
+
+function preencheCamposComCEP(data){
+    const logradouro = document.querySelector('[data-tipo="logradouro"]');
+    const cidade = document.querySelector('[data-tipo="cidade"]');
+    const estado = document.querySelector('[data-tipo="estado"]');
+
+    logradouro.value = data.logradouro;
+    cidade.value = data.localidade;
+    estado.value = data.uf;
+}
