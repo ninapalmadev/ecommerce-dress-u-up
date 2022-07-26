182299b2c1277ee8f52f7e941c6f9747686fb691
ninapalmadev <ninapalmadev@gmail.com>
2022-07-26 10:46:32 -0300

    refatora css do menu e da section chamada

--- js/app.js
+++ js/app.js
@@ -7,3 +7,12 @@ inputs.forEach(input => {
         valida(evento.target);
     })
 })
+
+const menuMobile = document.querySelector('[data-tipo="menu"]')
+
+function toggleMenu(){
+    const nav = document.querySelector('.navegacao__mobile');
+    nav.classList.toggle('active')
+}
+
+menuMobile.addEventListener('click', toggleMenu);
