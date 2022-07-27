764db680078e5fd7fe10aa6afd7ca14a63141ed9
ninapalmadev <ninapalmadev@gmail.com>
2022-07-27 17:40:04 -0300

    cria página de produtos e erro

--- js/produtos.js
+++ js/produtos.js
@@ -14,3 +14,28 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth
     })
 })
+
+const productImages = document.querySelectorAll(".produto-img img")
+const productImagesSlide = document.querySelector(".image-slider")
+
+let activeImageSlide = 0;
+
+productImages.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        productImages[activeImageSlide].classList.remove('active');
+        item.classList.add('active')
+        productImagesSlide.style.backgroundImage = `url('${item.src}')`;
+        activeImageSlide = i; 
+    })
+})
+
+const sizeBtns = document.querySelectorAll('.tamanho-radio-btn')
+let checkedBtn = 0;
+
+sizeBtns.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        sizeBtns[checkedBtn].classList.remove('check');
+        item.classList.add('check');
+        checkedBtn = i;
+    });
+});
