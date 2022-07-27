820e92546268b409a1a8935d66cd07f9217f963c
ninapalmadev <ninapalmadev@gmail.com>
2022-07-27 16:05:55 -0300

    cria aba de produtos

---
+++ js/produtos.js
@@ -0,0 +1,16 @@
+const productContainers = [...document.querySelectorAll('.produto-container')];
+const nxtBtn = [...document.querySelectorAll('.next-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensao = item.getBoundingClientRect();
+    let containerWidth = containerDimensao.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth
+    })
+})
